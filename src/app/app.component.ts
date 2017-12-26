871f4fb4cc4af4f7166e86153f61776517580d0c
Alexandr Gorban <ukrgorban@gmail.com>
2017-12-26 13:20:52 +0200

    animation #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    animations: [
+        trigger('clickedDiv', [
+            state('start', style({
+                backgroundColor: 'blue',
+                width: '150px',
+                height: '150px'
+            })),
+            state('end', style({
+                backgroundColor: 'red',
+                width: '300px',
+                height: '300px'
+            })),
+            state('active', style({
+                width: '170px',
+                height: '170px',
+                backgroundColor: 'yellow',
+            })),
+            transition('start <=> end', animate('800ms ease-out')),
+            transition('start => active', animate(300)),
+            transition('active => end', animate(300)),
+        ]),
+        trigger('multi', [
+            state('start', style({
+                width: '150px',
+                height: '150px',
+                border: '5px solid red'
+            })),
+            state('end', style({
+                width: '180px',
+                height: '180px',
+                backgroundColor: 'orange'
+            })),
+            transition('start <=> end', [
+                style({
+                    backgroundColor: 'black'
+                }),
+                animate(500)
+            ])
+        ])
+    ]
 })
 export class AppComponent {
+    clickedDivState = 'start';
+    multiState = 'start';
+    isVisible = false;
 
-    constructor() {}
-
+    changeDivState() {
+        this.clickedDivState = 'end';
+        setTimeout(() => {
+            this.clickedDivState = 'start';
+        }, 1000);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CarsComponent } from './cars/cars.component';
-import { AppRoutingModule } from './app-routing.module';
-
-import { CarsService } from './cars.service';
-import { CarComponent } from './car/car.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AuthService } from './auth.service';
-// import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CarsComponent,
-    CarComponent,
-    NotFoundComponent
   ],
   imports: [
       BrowserModule,
       FormsModule,
-      AppRoutingModule
+      BrowserAnimationsModule
   ],
-  providers: [CarsService, AuthService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
